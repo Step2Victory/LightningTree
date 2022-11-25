88da97dbafb19d0e3b34f9fcbae30794fd3fcb5d
= <stepanmalov555@gmail.com>
2022-11-25 10:01:33 +0300

    Add general functions and next iter procedures

--- ExternalField.h
+++ ExternalField.h
@@ -23,4 +23,44 @@ public:
 struct Vector
 {
     std::array<double, 3> data;
+
+    Vector operator-=(const Vector& rhs)
+    {
+        data[0] -= rhs.data[0];
+        data[1] -= rhs.data[1];
+        data[2] -= rhs.data[2];
+    }
+
+    Vector operator-(const Vector& rhs) const
+    {
+        return {{data[0] - rhs.data[0], data[1] - rhs.data[1], data[2] - rhs.data[2]}};
+    }
+
+    Vector operator+=(const Vector& rhs)
+    {
+        data[0] += rhs.data[0];
+        data[1] += rhs.data[1];
+        data[2] += rhs.data[2];
+    }
+
+    Vector operator+(const Vector& rhs) const
+    {
+        return {{data[0] + rhs.data[0], data[1] + rhs.data[1], data[2] + rhs.data[2]}};
+    }
+
+    double Dot(const Vector& rhs) const // DotProduct
+    {
+        return data[0] * rhs.data[0] + data[1] * rhs.data[1] + data[2] * rhs.data[2];
+    }
+
+    Vector operator-() const
+    {
+        return {-data[0], -data[1], -data[2]};
+    }
+
 };
+
+double Abs(const Vector& vector)
+{
+    return std::sqrt(vector.Dot(vector));
+}
--- LightningTree.h
+++ LightningTree.h
@@ -9,6 +9,11 @@ std::mt19937 gen(rd());
 std::uniform_real_distribution<> dis(0, 1.0);
 
 
+constexpr double epsilon_0 = 8.85418781762 * 1e-12;
+constexpr double pi = 3.1415926535;
+
+constexpr double kEps = 1e-9;
+
 class LightningTree
 {
     struct Charge
@@ -16,6 +21,10 @@ class LightningTree
         Vector point;
         double q;
         double Q;
+        Charge GetMirror()
+        {
+            return {.point={point.data[0], point.data[1], -point.data[2]}, .q=-q, .Q=-Q};
+        }
     };
 
     struct Edge
@@ -30,7 +39,8 @@ class LightningTree
     std::vector<Charge*> charges;
     
 
-    uint64_t iter_number;
+    uint64_t iter_number_charges;
+    uint64_t iter_number_edges;
 
     double h;
     double x_min;
@@ -46,7 +56,8 @@ class LightningTree
     double q_plus_max = 1;
     double q_minus_max = -1;
     double resistance = 1;
-    double E_plus;
+    double E_plus = 150'000;
+    double E_minus;
     double eta = 0;
     double beta = 0;
     double sigma;
@@ -55,37 +66,91 @@ class LightningTree
 
     double qCountPotential(const Charge& charge, const Vector& point)
     {
-        return 0;
+        return charge.q / (4 * pi * epsilon_0 * (Abs(charge.point - point) + r));
     }
 
     double qCountPotential(const std::vector<Charge*>& charge, const Vector& point)
     {
-        return 0;
+        double ans = 0;
+        for (size_t i = 0; i < charge.size(); ++i)
+        {
+            ans += qCountPotential(*charge[i], point) - qCountPotential(charge[i]->GetMirror(), point);
+        }
+        return ans;
     }
 
     double QCountPotential(const Charge& charge, const Vector& point)
     {
-        return 0;
+        return charge.Q / (4 * pi * epsilon_0 * (Abs(charge.point - point) + R));
     }
 
     double QCountPotential(const std::vector<Charge*>& charge, const Vector& point)
     {
-        return 0;
+        double ans = 0;
+        for (size_t i = 0; i < charge.size(); ++i)
+        {
+            ans += QCountPotential(*charge[i], point) - QCountPotential(charge[i]->GetMirror(), point);
+        }
+        return ans;
     }
 
     double ElectricFieldAlongEdge(const Edge* edge)
     {
-        return 0;
+        double l = Abs(edge->from->point - edge->to->point);
+        double phi_1 = qCountPotential(charges, edge->from->point) + QCountPotential(charges, edge->from->point);
+        double phi_2 = qCountPotential(charges, edge->to->point) + QCountPotential(charges, edge->to->point);
+        return -(phi_1 - phi_2) / l;
     }
 
     double CurrentAlongEdge(const Edge* edge)
     {
-        return 0;
+        return pi * r * r * edge->sigma * ElectricFieldAlongEdge(edge);
     }
 
     double ElectricFieldAlongEdgeRad(const Edge* edge)
     {
         return 0;
+    }
+
+    double Heaviside(double x)
+    {
+        if (x > 0)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    double CurrentSheath(const Charge& charge)
+    {
+        if (charge.q > 0)
+        {
+            return resistance * charge.q / abs(charge.q) * Heaviside((abs(charge.q) - q_plus_max) / r);
+        }
+        return resistance * charge.q / abs(charge.q) * Heaviside((abs(charge.q) - q_minus_max) / r);  
+    }
+
+    bool MakeEdge(Edge* edge)
+    {
+        double probability = dis(gen);
+        double E = ElectricFieldAlongEdge(edge);
+        if (E > 0)
+        {
+            return (1 - std::exp(-std::pow((E / E_plus), 2.5))) > probability;
+        }
+        return (1 - std::exp(-std::pow((E / E_minus), 2.5))) > probability;
+    }
+
+    bool Find(Charge * charge, std::vector<Edge*> edges)
+    {
+        for (auto * edge : edges)
+        {
+            if (Abs(edge->to->point - charge->point) < kEps)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
 public:
@@ -98,22 +163,80 @@ public:
 
     : h(h), x_min(x_min), y_min(y_min), z_min(z_min), x_max(x_max), y_max(y_max), z_max(z_max), delta_t(delta_t), delta_T(delta_T),
     r(r), R(R), q_plus_max(q_plus_max), q_minus_max(q_minus_max), resistance(resistance), E_plus(E_plus), eta(eta), beta(beta),
-    sigma(sigma), phi_a(phi_a), iter_number(0)
-    {}
+    sigma(sigma), phi_a(phi_a), iter_number_charges(0), iter_number_edges(0)
+    {
+        E_minus = 2 * E_plus;
+    }
 
 
     void NextIter() // combine charges and edges count
-    {}
+    {
+        NextIterCharges();
+        if (delta_t * iter_number_charges > delta_T * iter_number_edges)
+        {   
+            NextIterEdges();
+        }
+    }
 
     void NextIterCharges() // count new charges
-    {}
+    {
+        std::map<Charge*, std::pair<double, double>> delta_charges;
+        for (auto elem : graph)
+        {
+            Charge* charge = elem.first;
+            std::vector<Edge*> edges = elem.second;
+            for (auto* edge: edges)
+            {
+                delta_charges[charge].first += CurrentAlongEdge(edge);
+            }
+            delta_charges[charge].first-= CurrentSheath(*charge);
+            delta_charges[charge].second = CurrentSheath(*charge);
+        }
+        for (auto elem : delta_charges)
+        {
+            elem.first->q += delta_charges[elem.first].first * delta_t;
+            elem.first->Q += delta_charges[elem.first].second * delta_t;
+        }
+        iter_number_charges++;
+    }
     
     void NextIterEdges() // count new edges using dis
     {
-        
+
+        for (auto elem : graph)
+        {
+            Charge* charge = elem.first;
+            std::vector<Edge*> edges = elem.second;
+            for (int i = -1; i < 2; ++i)
+            {
+                for (int j = -1; j < 2; ++j)
+                {
+                    for (int k = -1; k < 2; ++k)
+                    {
+                        Charge * new_charge = new Charge{.point = charge->point + Vector{i * h, j * h, k * h}, .q = 0, .Q = 0};
+                        Edge * edge = new Edge{.from = charge, .to = new_charge, .sigma = sigma};
+                        if (!Find(new_charge, edges) && MakeEdge(edge))
+                        {
+                            charges.push_back(new_charge);
+                            graph[charge].push_back(edge);
+                            graph[new_charge].push_back(new Edge{.from = new_charge, .to = charge, .sigma = sigma});
+                        }
+                        else
+                        {
+                            delete edge;
+                            delete new_charge;
+                        }
+                    }
+                }
+            }
+        }
+        iter_number_edges++;
     }    
 
-    std::map<Charge*, std::vector<Edge*>> GetGraph() const;
+    std::map<Charge*, std::vector<Edge*>> GetGraph() const
+    {
+        return graph;
+    }
 
     ~LightningTree()
     {
