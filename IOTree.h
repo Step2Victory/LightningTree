2424c72e2d4353be9b29ad1d2cb08d8df6c365e3
NikolayVolzhanskiy <adheredn@gmail.com>
2022-11-26 13:19:33 +0300

    Update IOTree.h

--- IOTree.h
+++ IOTree.h
@@ -1,13 +1,34 @@
 #include "LightningTree.h"
 #include <fstream>
+#include <iostream>
 
+using namespace std;
 
 LightningTree GetLightningTreeFromFile(std::string filename)
 {
-    return {};
+    ifstream fin(filename, ios_base::in);
+ 
+    if (!fin.is_open()) // если файл не открыт
+        cout << "Файл не может быть открыт!\n"; // сообщить об этом
+    else
+    {
+
+    /// char buff[50]; // буфер промежуточного хранения считываемого из файла текста
+    /// fin >> buff; // считали первое слово из файла
+    /// fin.getline(buff, 50); // считали строку из файла
+    /// fin.close(); // закрываем файл
+
+    /*
+    LightningTree* tree = ParseTree(fin);
+    */
+
+    }
+    return {/*tree*/};
 }
 
 void WriteInFile(const LightningTree& tree)
 {
-    
+    ofstream fout("LightningTree.txt"); 
+    //fout << ParseToString(tree);
+    fout.close();
 }
