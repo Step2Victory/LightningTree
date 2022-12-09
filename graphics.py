17ffac0bca459b5107f27ba4ff35db08ac74aad2
NikolayVolzhanskiy <66516547+NikolayVolzhanskiy@users.noreply.github.com>
2022-12-10 00:25:03 +0300

    Writed lib Plotly with Jupyter-Dash
    Внёс метод построение графа через библиотеку Pyplot.
    Запуск из блокноты выводит Dashboard в ленту.
    Запуск из файла python создаёт ссылку на  dashboard

--- graphics.py
+++ graphics.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
-import copy
+import plotly.graph_objects as go
+import pandas as pd
+
+from jupyter_dash import JupyterDash
+from dash import dcc, html
 
 
 class Vector(object):
@@ -10,6 +14,13 @@ class Vector(object):
         
     def __str__(self):
         return "({}, {}, {})".format(self.x, self.y, self.z)
+    
+    # def __iter__(self):
+    #     return self
+    
+    # def __next__(self):
+    #     return self
+        
 
 
 class Charge(object):
@@ -59,6 +70,8 @@ def map3d_tree(edges):
     Q = []
     #print(edges[1]._from)
     for edge in edges:
+        # x, y, z = edge._from.point // нужен iter и next
+        # x, y, z = edge._to.point
         x.append(edge._from.point.x)
         x.append(edge._to.point.x)
         y.append(edge._from.point.y)
@@ -122,11 +135,129 @@ def map2d_tree(edges, plane:str = "xz", var:float = 0):
             ax.scatter(x, y, c=Q, cmap="plasma") 
     plt.show()
     
+
+def map3d_tree_plotly(edges, plane:str = "", value:int=0, Range:list=[-10, 10]):
+    app = JupyterDash('SimpleExemple')
+    place = plane.upper()
+    edge_x, edge_y, edge_z = [], [], []
+    node_x, node_y, node_z = [], [], []
+    node_q, node_Q = [], []
+    for edge in edges:
+        edge_x.append(edge._from.point.x)
+        edge_y.append(edge._from.point.y)
+        edge_z.append(edge._from.point.z)
+        
+        node_x.append(edge._from.point.x)
+        node_y.append(edge._from.point.y)
+        node_z.append(edge._from.point.z)
+        
+        node_q.append(edge._from.q)
+        node_Q.append(edge._from.Q)
+        
+        edge_x.append(edge._to.point.x)
+        edge_y.append(edge._to.point.y)
+        edge_z.append(edge._to.point.z)
+        
+        node_x.append(edge._to.point.x)
+        node_y.append(edge._to.point.y)
+        node_z.append(edge._to.point.z)
+        
+        node_q.append(edge._to.q); 
+        node_Q.append(edge._to.Q); 
+          
+        edge_x.append(None)
+        edge_y.append(None)
+        edge_z.append(None)
+        
+    df_edges = pd.DataFrame({"x": edge_x, "y": edge_y, "z": edge_z})
+    df_nodes = pd.DataFrame({"x": node_x, "y": node_y, "z": node_z, "q": node_q, "Q": node_Q})
+
+    setting = {'showbackground':False, 'showticklabels':False, 'showgrid':False, 'zeroline':False}
+    layout = go.Layout(showlegend = False, hovermode = 'closest',
+                       scene = {'xaxis':setting, 'yaxis':setting, 'zaxis':setting})
+    
+    if (value!=0 or Range!=[-10, 10]) and place == "":
+        print("Не введена плоскость!") 
+     
+    if place != "":
+        if (value!=0) and (Range==[-10, 10]):
+            Range = [Range[0] + value, Range[1] + value]
+            
+        low, high = Range
+        mask_nodes = True
+        mask_edges = True    
+        match place:
+            case "XZ" | "ZX":
+                mask_nodes = (df_nodes.y >= low) & (df_nodes.y <= high)
+                mask = []
+                for i in range(0, len(df_edges.y), 3):
+                    if (low <= df_edges.y[i] <= high) and (low <= df_edges.y[i+1] <= high):
+                        mask.append(True) 
+                        mask.append(True)
+                        mask.append(True)
+                    else:
+                        mask.append(False)
+                        mask.append(False)
+                        mask.append(False)
+                mask_edges = pd.Series(data=mask)
+            case "YZ" | "ZY":
+                mask_nodes = (df_nodes.x >= low) & (df_nodes.x <= high)
+                mask = []
+                for i in range(0, len(df_edges.x), 3):
+                    if (low <= df_edges.x[i] <= high) and (low <= df_edges.x[i+1] <= high):
+                        mask.append(True)
+                        mask.append(True)
+                        mask.append(True)
+                    else:
+                        mask.append(False)
+                        mask.append(False)
+                        mask.append(False)
+                mask_edges = pd.Series(data=mask)
+            case "XY" | "YX":
+                mask_nodes = (df_nodes.z >= low) & (df_nodes.z <= high)
+                mask = []
+                for i in range(0, len(df_edges.z), 3):
+                    if (low <= df_edges.z[i] <= high) and (low <= df_edges.z[i+1] <= high):
+                        mask.append(True)
+                        mask.append(True)
+                        mask.append(True)
+                    else:
+                        mask.append(False)
+                        mask.append(False)
+                        mask.append(False)
+                mask_edges = pd.Series(data=mask)
+        
+        df_edges = df_edges[mask_edges]
+        df_nodes = df_nodes[mask_nodes]
+        
+    edge_trace = go.Scatter3d(x=df_edges.x, y=df_edges.y, z=df_edges.z, 
+                            line = dict(width=3, color='blue'), hoverinfo='none', mode='lines')
+    
+    node_trace = go.Scatter3d(x=df_nodes.x, y=df_nodes.y, z=df_nodes.z, 
+                            mode='markers', 
+                            hoverinfo='text', 
+                            marker=dict(color=[],
+                                        showscale=True, 
+                                        colorscale='Electric',
+                                        size=3), 
+                                        line_width=.1)
+    node_trace.text = df_nodes.Q
+    
+    data = [edge_trace, node_trace]
+    fig = go.Figure(data=data, layout=layout)
+    app.layout = html.Div([
+        html.H1("Граф дерева молнии", style={'textAlign': 'center', 'color':'gold'}),
+        dcc.Graph(figure=fig, style={'height':'100vh'})
+        ])
+        
+    app.run_server(mode='inline')
+    
     
 def main():
     edges = openTree("LightningTree.txt")
-    map3d_tree(edges)
+    #map3d_tree(edges)
     #map2d_tree(edges)
+    map3d_tree_plotly(edges, "xz", Range=[-100, 0])
 
 
 if __name__ == '__main__':
