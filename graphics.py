e4bdcd4354e1e6f0564760bd3045c0e6f8370beb
NikolayVolzhanskiy <adheredn@gmail.com>
2022-12-29 18:47:00 +0300

    Update graphics
    Изменил расцветку графа и добавил способность обновлять граф в реальном времени

--- graphics.py
+++ graphics.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from jupyter_dash import JupyterDash
 from dash import dcc, html
+from dash.dependencies import Output, Input
 
 
 class Vector(object):
@@ -59,89 +60,9 @@ def openTree(filename):
             charges.clear()
     return edges
 
-
-def map3d_tree(edges):
-    # create 3d axes
-    ax = plt.axes(projection='3d')
-    ax.set_title("3D graph lighting tree")
-    x = []
-    y = []
-    z = []
-    q = []
-    Q = []
-    # print(edges[1]._from)
-    for edge in edges:
-        # x, y, z = edge._from.point // нужен iter и next
-        # x, y, z = edge._to.point
-        x.append(edge._from.point.x)
-        x.append(edge._to.point.x)
-        y.append(edge._from.point.y)
-        y.append(edge._to.point.y)
-        z.append(edge._from.point.z)
-        z.append(edge._to.point.z)
-        q.append(edge._from.q)
-        q.append(edge._to.q)
-        Q.append(edge._from.Q)
-        Q.append(edge._to.Q)
-        ax.plot([edge._from.point.x, edge._to.point.x],
-                [edge._from.point.y, edge._to.point.y],
-                [edge._from.point.z, edge._to.point.z], color="blue", alpha=0.4)
-
-    ax.scatter(x, y, z, c=Q, cmap="plasma", alpha=0.2)
-    plt.show()
-
-
-def map2d_tree(edges, plane: str = "xz", var: float = 0):
-    ax = plt.axes()
-    ax.set_title("2D graph lighting tree in plane " + plane)
-    x = []
-    y = []
-    z = []
-    q = []
-    Q = []
-    for edge in edges:
-        x.append(edge._from.point.x)
-        x.append(edge._to.point.x)
-        y.append(edge._from.point.y)
-        y.append(edge._to.point.y)
-        z.append(edge._from.point.z)
-        z.append(edge._to.point.z)
-        q.append(edge._from.q)
-        q.append(edge._to.q)
-        Q.append(edge._from.Q)
-        Q.append(edge._to.Q)
-
-        match plane:
-            case "xz" | "zx":
-                if (edge._from.point.y == var and edge._to.point.y == var):
-                    ax.plot([edge._from.point.x, edge._to.point.x],
-                            [edge._from.point.z, edge._to.point.z],
-                            color="blue")
-            case "yz" | "zy":
-                if (edge._from.point.y == var and edge._to.point.y == var):
-                    ax.plot([edge._from.point.y, edge._to.point.y],
-                            [edge._from.point.z, edge._to.point.z],
-                            color="blue")
-            case "xy" | "yx":
-                if (edge._from.point.y == var and edge._to.point.y == var):
-                    ax.plot([edge._from.point.x, edge._to.point.x],
-                            [edge._from.point.y, edge._to.point.y],
-                            color="blue")
-
-    match plane:
-        case "xz" | "zx":
-            ax.scatter(x, z, c=Q, cmap="plasma")
-        case "yz" | "zy":
-            ax.scatter(y, z, c=Q, cmap="plasma")
-        case "xy" | "yx":
-            ax.scatter(x, y, c=Q, cmap="plasma")
-    plt.show()
-
-
-def map3d_tree_plotly(edges, plane: str = "", value: int = 0, Range: list=[-10, 10], _mode:str='external'):
-    app = JupyterDash('SimpleExemple')
-    place = plane.upper()
+def parseTreeToDF(edges):
     edge_x, edge_y, edge_z = [], [], []
+    edge_color = []
     node_x, node_y, node_z = [], [], []
     node_q, node_Q = [], []
     for edge in edges:
@@ -166,23 +87,27 @@ def map3d_tree_plotly(edges, plane: str = "", value: int = 0, Range: list=[-10, 
 
         node_q.append(edge._to.q)
         node_Q.append(edge._to.Q)
+        
+        if(edge._to.point.z - edge._from.point.z > 0):
+            edge_color.append(1)
+            edge_color.append(1)
+        else:
+            edge_color.append(-1)
+            edge_color.append(-1)
 
         edge_x.append(None)
         edge_y.append(None)
         edge_z.append(None)
+        edge_color.append(None)
 
-    df_edges = pd.DataFrame({"x": edge_x, "y": edge_y, "z": edge_z})
+    df_edges = pd.DataFrame({"x": edge_x, "y": edge_y, "z": edge_z, 'color': edge_color})
     df_nodes = pd.DataFrame({"x": node_x, "y": node_y, "z": node_z, "q": node_q, "Q": node_Q})
+    
+    return df_edges, df_nodes
 
-    setting = {'showbackground': False, 'showticklabels': False, 'showgrid': False, 'zeroline': False}
-    layout = go.Layout(showlegend=False, hovermode='closest',
-                       scene={'xaxis': setting, 'yaxis': setting, 'zaxis': setting})
-
-    if (value != 0 or Range != [-10, 10]) and place == "":
-        print("Не введена плоскость!")
-
+def slice_selection(df_edges, df_nodes, place, value, Range):       
     if place != "":
-        if (value != 0) and (Range == [-10, 10]):
+        if (Range == [-10, 10]) and (value != 0):
             Range = [Range[0] + value, Range[1] + value]
 
         low, high = Range
@@ -228,42 +153,166 @@ def map3d_tree_plotly(edges, plane: str = "", value: int = 0, Range: list=[-10, 
                         mask.append(False)
                         mask.append(False)
                 mask_edges = pd.Series(data=mask)
-
         df_edges = df_edges[mask_edges]
-        df_nodes = df_nodes[mask_nodes]
+        df_nodes = df_nodes[mask_nodes]   
+    elif value != 0 or Range != [-10, 10]:
+        print("Не введена плоскость!")
+        
+    return df_edges, df_nodes
+    
+# def map3d_tree(edges):
+#     # create 3d axes
+#     ax = plt.axes(projection='3d')
+#     ax.set_title("3D graph lighting tree")
+#     x = []
+#     y = []
+#     z = []
+#     q = []
+#     Q = []
+#     # print(edges[1]._from)
+#     for edge in edges:
+#         # x, y, z = edge._from.point // нужен iter и next
+#         # x, y, z = edge._to.point
+#         x.append(edge._from.point.x)
+#         x.append(edge._to.point.x)
+#         y.append(edge._from.point.y)
+#         y.append(edge._to.point.y)
+#         z.append(edge._from.point.z)
+#         z.append(edge._to.point.z)
+#         q.append(edge._from.q)
+#         q.append(edge._to.q)
+#         Q.append(edge._from.Q)
+#         Q.append(edge._to.Q)
+#         ax.plot([edge._from.point.x, edge._to.point.x],
+#                 [edge._from.point.y, edge._to.point.y],
+#                 [edge._from.point.z, edge._to.point.z], color="blue", alpha=0.4)
 
-    edge_trace = go.Scatter3d(x=df_edges.x, y=df_edges.y, z=df_edges.z,
-                              line=dict(width=2, color='lightblue'), hoverinfo='none', mode='lines')
+#     ax.scatter(x, y, z, c=Q, cmap="plasma", alpha=0.2)
+#     plt.show()
 
-    node_trace = go.Scatter3d(x=df_nodes.x, y=df_nodes.y, z=df_nodes.z,
-                              mode='markers',
-                              hoverinfo='text',
-                              marker=dict(showscale=True,
-                                          colorscale='viridis',
-                                          color=df_nodes.Q,
-                                          cmin=-1,
-                                          cmax=1,
-                                          size=2                                       
-                                          ),
-                              line_width=.1
-                              )
-    node_trace.text = df_nodes.Q
+# def map2d_tree(edges, plane: str = "xz", var: float = 0):
+#     ax = plt.axes()
+#     ax.set_title("2D graph lighting tree in plane " + plane)
+#     x = []
+#     y = []
+#     z = []
+#     q = []
+#     Q = []
+#     for edge in edges:
+#         x.append(edge._from.point.x)
+#         x.append(edge._to.point.x)
+#         y.append(edge._from.point.y)
+#         y.append(edge._to.point.y)
+#         z.append(edge._from.point.z)
+#         z.append(edge._to.point.z)
+#         q.append(edge._from.q)
+#         q.append(edge._to.q)
+#         Q.append(edge._from.Q)
+#         Q.append(edge._to.Q)
 
-    data = [edge_trace, node_trace]
-    fig = go.Figure(data=data, layout=layout)
-    app.layout = html.Div([
-        html.H1("Граф дерева молнии", style={'textAlign': 'center', 'color': 'gold'}),
-        dcc.Graph(figure=fig, style={'height': '120vh'})
-    ])
+#         match plane:
+#             case "xz" | "zx":
+#                 if (edge._from.point.y == var and edge._to.point.y == var):
+#                     ax.plot([edge._from.point.x, edge._to.point.x],
+#                             [edge._from.point.z, edge._to.point.z],
+#                             color="blue")
+#             case "yz" | "zy":
+#                 if (edge._from.point.y == var and edge._to.point.y == var):
+#                     ax.plot([edge._from.point.y, edge._to.point.y],
+#                             [edge._from.point.z, edge._to.point.z],
+#                             color="blue")
+#             case "xy" | "yx":
+#                 if (edge._from.point.y == var and edge._to.point.y == var):
+#                     ax.plot([edge._from.point.x, edge._to.point.x],
+#                             [edge._from.point.y, edge._to.point.y],
+#                             color="blue")
 
-    app.run_server(mode=_mode)  # inline - внутри jupyter; external - в браузере
+#     match plane:
+#         case "xz" | "zx":
+#             ax.scatter(x, z, c=Q, cmap="plasma")
+#         case "yz" | "zy":
+#             ax.scatter(y, z, c=Q, cmap="plasma")
+#         case "xy" | "yx":
+#             ax.scatter(x, y, c=Q, cmap="plasma")
+#     plt.show()
+
+
+def map3d_tree_plotly(filename, place: str = "", value: int = 0, Range: list=[-10, 10], mode:str='external', interval:float=5):
+    app = JupyterDash('SimpleExemple')
+    place = place.upper()
+    scale_nodes = [(0, "darkblue"), (0.15, "blue"), (0.49, "yellow"), (0.5, "gray"), (0.51, "yellow"), (0.85, "red"), (1, "darkred")] # цветовая шкала для зарядов
+    scale_case = [(0, "darkblue"), (0.15, "blue"), (0.49, "yellow"), (0.5, "white"), (0.51, "yellow"), (0.85, "red"), (1, "darkred")] # цветовая шкала для чехлов
+    setting = {'showbackground': False, 'showticklabels': False, 'showgrid': False, 'zeroline': False}
+    layout = go.Layout(showlegend=False, hovermode='closest',
+                       scene={'xaxis': setting, 'yaxis': setting, 'zaxis': setting}
+                       )
+    
+    app.layout = html.Div([html.H1("Граф дерева молнии", 
+                                   style={'textAlign': 'center', 'color': 'gold'}),
+                           dcc.Graph(id='live-update-graph',
+                                     style={'height': '100vh'}),
+                           dcc.Interval(id='interval-component',
+                                        interval=interval*1000,
+                                        n_intervals=0)
+                           ])
+    
+    @app.callback(Output('live-update-graph', 'figure'),
+                  Input('interval-component', 'n_intervals'))
+    def update_graph_live(n):
+        edges = openTree(filename)
+        # Разбор файла
+        df_edges, df_nodes = parseTreeToDF(edges)
+        # Выделение среза среза, если задана плоскость
+        df_edges, df_nodes = slice_selection(df_edges, df_nodes, place, value, Range)
+                        
+        # Построение рёбер
+        edge_trace = go.Scatter3d(x=df_edges.x, y=df_edges.y, z=df_edges.z,
+                                line=dict(width=2, color=df_edges.color, colorscale=["darkslateblue", "crimson"], cmin=-1, cmax=1), 
+                                hoverinfo='none', 
+                                mode='lines'
+                                )
+        
+        # Построение зарядов    
+        node_trace = go.Scatter3d(x=df_nodes.x, y=df_nodes.y, z=df_nodes.z,
+                                mode='markers',
+                                marker=dict(showscale=True,
+                                            colorscale=scale_nodes,
+                                            color=df_nodes.Q,
+                                            cmin=-0.01,
+                                            cmax=0.01,
+                                            size=2.4,
+                                            ),
+                                line_width=.1
+                                )
+        
+        # Построение чехлов
+        case_trace = go.Scatter3d(x=df_nodes.x, y=df_nodes.y, z=df_nodes.z,
+                                mode='markers',
+                                text = df_nodes.q,
+                                customdata= df_nodes.Q,
+                                hovertemplate='q= %{customdata} <br>Q= %{text}<extra></extra>',
+                                marker=dict(showscale=False,
+                                            colorscale=scale_case,
+                                            color=df_nodes.q,
+                                            cmin=-0.0001,
+                                            cmax=0.0001,
+                                            size=24
+                                            ),
+                                line_width=1,
+                                opacity=0.1
+                                )
+        
+        data = [edge_trace, node_trace, case_trace]
+        fig = go.Figure(data=data, layout=layout)   
+             
+        return fig
+
+    app.run_server(mode=mode)  # inline - внутри jupyter; external - в браузере
 
 
 def main():
-    edges = openTree("LightningTree.txt")
-    # map3d_tree(edges)
-    # map2d_tree(edges)
-    map3d_tree_plotly(edges)#, "xz", Range=[-100, 0])
+    # edges = openTree("LightningTree.txt")
+    map3d_tree_plotly("LightningTree.txt")#, "xz", Range=[-100, 0])
 
 
 if __name__ == '__main__':
