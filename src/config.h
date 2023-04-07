2f0c39226176b13408e31c232d76d96069954ead
Takumeika <alexiy-1996@mail.ru>
2023-04-07 22:46:21 +0300

    return config.h

---
+++ src/config.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+#include <filesystem>
+
+inline std::filesystem::path current_path = "C:\\Users\\swat5\\OneDrive\\Документы\\По учёбе\\Диплом\\LightningTree";
+inline std::filesystem::path path_data = current_path / "LightningTree_data";
