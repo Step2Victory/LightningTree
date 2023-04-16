2c76b4fc67d3a8ac984fc76751ad67aa33153594
Vol`Nik <adheredn@gmail.com>
2023-04-16 14:57:56 +0300

    update graphics.py
    Обновил построение приложения dash.
    Добавил временную шкалу и кнопку для остановки интервального обновления.

--- graphics.py
+++ graphics.py
@@ -4,7 +4,7 @@ import math
 
 from plotly.subplots import make_subplots
 from jupyter_dash import JupyterDash
-from dash import dcc, html, Output, Input
+from dash import dcc, html, Output, Input, State, ctx
 
 # from dash.dependencies import Output, Input
 
@@ -56,6 +56,8 @@ class LightningTree(object):
         # self.df_q_history = self.open_file(folder+'/q_history_1.txt')
         # self.df_Q_history = self.open_file(folder+'/Q_history.txt')
 
+    def __str__(self):
+        return "({}, {})".format(self.figure_tree, self.figure_plots)
 
     def open_file(self, filename: str) -> pd.DataFrame:
         """
@@ -165,6 +167,9 @@ class LightningTree(object):
     
 
     def plots(self):
+        """
+        Создания словаря графических объектов из 2D графиков
+        """
         self.figure_plots = {
             "sum" : self.plot([self.distribution(self.df_vertex, 'z', 'sum', 'q'), 
                                 self.distribution(self.df_vertex, 'z', 'sum', 'Q')]),
@@ -188,11 +193,7 @@ class LightningTree(object):
 
     def plot_tree(self):# -> go.Figure:
         """
-        Создание 3D графа дерева молнии
-        
-        Parametrs
-        ---------
-        return: Графический объект
+        Создание графического объекта 3D графа дерева молнии
         """
         # Настройки для отображения графиков
         scale_nodes = [(0, "darkblue"), (0.15, "blue"), (0.49, "yellow"), (0.5, "gray"), (0.51, "yellow"), (0.85, "red"), (1, "darkred")] # цветовая шкала для зарядов
@@ -255,106 +256,99 @@ def run(folder:str, mode:str='external', interval:int=0):
     """
     # Создание Dash-приложения
     app = JupyterDash('SimpleExemple')
+    disable = False
 
     lt_history = [LightningTree(folder)]
     lt_history[0].plot_tree()
     lt_history[0].plots()
-    # Настройка и запуск Dash-приложения
 
-    # app.layout = html.Div([html.H1("Модель молнии", style={'textAlign': 'center', 'color': 'gold'}),
-                        #    html.Div([html.H4("Распределение заряда по высоте", style={'textAlign': 'center'}),
-                        #              dcc.Graph(figure=(self.plot([self.distribution(self.df_vertex, 'z', 'sum', 'q'), 
-                        #                                           self.distribution(self.df_vertex, 'z', 'sum', 'Q')])),
-                        #                        style={'height': '90vh'})],
-                        #            style={'display': 'inline-block', 'width': '40%'}),
-                        #    html.Div([html.H4("Граф дерева", style={'textAlign': 'center'}),
-                        #             dcc.Graph(figure=(self.plot_tree()), style={'height': '90vh'})],
-                        #            style={'display': 'inline-block', 'width': '60%'})])
-    
-    # app.layout = html.Div([html.H1("Модель молнии", style={'textAlign': 'center', 'color': 'gold'}),
-                        #    html.Div([html.H4("Распределение заряда по высоте", style={'textAlign': 'center'}),
-                        #              dcc.Graph(figure=(self.plot([self.distribution(self.df_vertex, 'z', 'sum', 'q'), 
-                        #                                           self.distribution(self.df_vertex, 'z', 'sum', 'Q'),
-                        #                                           self.fi_def(self.df_vertex)])),
-                        #                        style={'height': '90vh'})],
-                        #            style={'display': 'inline-block', 'width': '40%'}),
-                        #    html.Div([html.H4("Граф дерева", style={'textAlign': 'center'}),
-                        #             dcc.Graph(figure=(self.plot_tree()), style={'height': '90vh'})],
-                        #            style={'display': 'inline-block', 'width': '60%'})])
     if interval == 0:
-        app.layout = html.Div([html.H1("Модель молнии", style={'textAlign': 'center', 'color': 'gold'}),
-                                
-                                html.Div([html.H4("Распределение заряда по высоте", style={'textAlign': 'center'}),
-                                            
-                                            dcc.Dropdown(options=[{'label':"Распределение суммы зарядов по высоте", 'value':'sum'}, 
-                                                        {'label': "Распределение среднего значения зарядов по высоте", 'value':'avg'},
-                                                    {'label': "Распределение полного потенциала по высоте", 'value':'full_phi'},
-                                                    {'label': "Распределение потенциала внешнего поля по высоте", 'value':'ext_phi'},
-                                                    {'label': "Распределение токов по высоте", "value" : "current"},
-                                                    {'label': "Все графики", 'value':'all'}, {'label': "По умолчанию", 'value':'default'}], value='default', id='dropdown'),
+        disable = True
 
-                                            dcc.Graph(id='graph_distrib',
-                                                #    figure=(self.plot([self.distribution(self.df_vertex, 'z', 'sum', 'q'), 
-                                                #                       self.distribution(self.df_vertex, 'z', 'sum', 'Q'),
-                                                #                       self.fi_def(self.df_vertex)])),
-                                                    style={'height': '90vh'})],
-                                                    style={'display': 'inline-block', 'width': '39%'}),
+    # Настройка и запуск Dash-приложения в зависимости от параметра
+    app.layout = html.Div([html.H1("Модель молнии", style={'textAlign': 'center', 'color': 'gold'}),
+                        
+                        dcc.Interval(id='interval-component', interval=interval*1000, n_intervals=0, disabled=disable, max_intervals=20),
 
-                                html.Div([html.H4("Граф дерева", style={'textAlign': 'center'}),
-                                            
-                                        dcc.Graph(figure=(lt_history[-1].figure_tree), id='graph_tree', style={'height': '90vh'})],
+                        html.Div([html.H4("Распределение заряда по высоте", style={'textAlign': 'center'}),
+                                    
+                                    dcc.Dropdown(options=[{'label':"Распределение суммы зарядов по высоте", 'value':'sum'}, 
+                                                {'label': "Распределение среднего значения зарядов по высоте", 'value':'avg'},
+                                                {'label': "Распределение полного потенциала по высоте", 'value':'full_phi'},
+                                                {'label': "Распределение потенциала внешнего поля по высоте", 'value':'ext_phi'},
+                                                {'label': "Распределение токов по высоте", "value" : "current"},
+                                                {'label': "Все графики", 'value':'all'}, {'label': "По умолчанию", 'value':'default'}], value='default', id='dropdown'),
 
-                                        style={'display': 'inline-block', 'width': '59%'})])
-    else:
-        app.layout = html.Div([html.H1("Модель молнии", style={'textAlign': 'center', 'color': 'gold'}),
-                            
-                            dcc.Interval(id='interval-component', interval=interval*1000, n_intervals=0),
+                                    dcc.Graph(#figure=lt_history[-1].figure_plots['default'], 
+                                              id='graph_distrib', style={'height': '80vh'})],
+                                    
+                                    style={'display': 'inline-block', 'width': '39%'}),
 
-                            html.Div([html.H4("Распределение заряда по высоте", style={'textAlign': 'center'}),
-                                        
-                                        dcc.Dropdown(options=[{'label':"Распределение суммы зарядов по высоте", 'value':'sum'}, 
-                                                    {'label': "Распределение среднего значения зарядов по высоте", 'value':'avg'},
-                                                    {'label': "Распределение полного потенциала по высоте", 'value':'full_phi'},
-                                                    {'label': "Распределение потенциала внешнего поля по высоте", 'value':'ext_phi'},
-                                                    {'label': "Распределение токов по высоте", "value" : "current"},
-                                                    {'label': "Все графики", 'value':'all'}, {'label': "По умолчанию", 'value':'default'}], value='default', id='dropdown'),
+                        html.Div([html.H4("Граф дерева", style={'textAlign': 'center'}),
+                                    
+                                    dcc.Graph(figure=lt_history[-1].figure_tree, id='graph_tree', style={'height': '80vh'})],
 
-                                        dcc.Graph(id='graph_distrib',
-                                            #    figure=(self.plot([self.distribution(self.df_vertex, 'z', 'sum', 'q'), 
-                                            #                       self.distribution(self.df_vertex, 'z', 'sum', 'Q'),
-                                            #                       self.fi_def(self.df_vertex)])),
+                                    style={'display': 'inline-block', 'width': '59%'}),
 
-                                                style={'height': '90vh'})],
-                                                style={'display': 'inline-block', 'width': '39%'}),
+                        html.Div(dcc.Slider(0, 1, step = 1, id='time_slider', disabled=disable),
+                                 
+                                 style={'display': 'inline-block', 'width': '95%'}),
 
-                            html.Div([html.H4("Граф дерева", style={'textAlign': 'center'}),
-                                        
-                                    dcc.Graph(figure=(lt_history[-1].figure_tree), id='graph_tree', style={'height': '90vh'})],
-
-                                    style={'display': 'inline-block', 'width': '59%'})])
-        
-    
-    @app.callback(Output('graph_distrib', 'figure'),
-                  Input('dropdown', 'value'))
-    def update_figure(value):
-
-        return lt_history[-1].figure_plots[value]
+                        html.Div(html.Button('Start/Stop', id='button'),
+                                 
+                                 style={'display': 'inline-block', 'width': '3%'})
+                        ])
     
 
     @app.callback(Output('graph_tree', 'figure'),
-                    Input('interval-component', 'n_intervals'))
-    def update_graph_live(n):
-        lt_history.append(LightningTree(folder))
-        lt_history[-1].plot_tree()
-        lt_history[-1].plots()
+                  Output('time_slider', 'max'),
+                  Input('interval-component', 'n_intervals'),
+                  Input('time_slider', 'value'))
+    def update_graph_live(n, t):
+        if n is None:
+            raise JupyterDash.exceptions.PreventUpdate
+        
+        time = -1
+        
+        if ctx.triggered_id == 'interval-component':
+            lt_history.append(LightningTree(folder))
+            lt_history[-1].plot_tree()
+            lt_history[-1].plots()
 
-        return lt_history[-1].figure_tree
+        if ctx.triggered_id == 'time_slider':
+            time = t
+
+        # print("{} / {}".format(len(lt_history), n))
+
+        return lt_history[time].figure_tree, len(lt_history)-1
+    
+
+    @app.callback(Output('graph_distrib', 'figure'),
+                  Input('dropdown', 'value'),
+                  Input('interval-component', 'n_intervals'),
+                  Input('time_slider', 'value'))
+    def update_figure(value, n, t):
+        time = -1
+
+        if ctx.triggered_id == 'time_slider':
+            time = t
+        
+        return lt_history[time].figure_plots[value]
+    
+
+    @app.callback(Output('interval-component', 'disabled'),
+                  Input('button', 'n_clicks'),
+                  State('interval-component', 'disabled'))
+    def toggle_interval(click, disabled):
+        if click:
+            return not disabled
+        return disabled
     
     app.run_server(mode=mode)
 
 
 def main():
-    run("LightningTree_data")
+    run("LightningTree_data", interval=1)
     # lt.run()#interval=1)
     # print(lt.distribution(lt.df_vertex, 'z', 'sum', 'q'))
     # print(lt.fi_def(lt.df_vertex))
